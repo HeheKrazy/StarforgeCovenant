a73e6e2783290d4b761051d3bf87e9af042597d1
HeheKrazy <AdamSabo2750@Gmail.com>
2026-03-09 20:13:39 -0400

    Enemy Interface

--- Source/StarforgeCovenant/Private/Characters/MySFC_EnemyBase.cpp
+++ Source/StarforgeCovenant/Private/Characters/MySFC_EnemyBase.cpp
@@ -3,3 +3,10 @@
 
 #include "Characters/MySFC_EnemyBase.h"
 
+void AMySFC_EnemyBase::HighlightActor()
+{
+}
+
+void AMySFC_EnemyBase::UnHighlightActor()
+{
+}
--- Source/StarforgeCovenant/Public/Characters/MySFC_EnemyBase.h
+++ Source/StarforgeCovenant/Public/Characters/MySFC_EnemyBase.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Characters/SFC_CharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "MySFC_EnemyBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class STARFORGECOVENANT_API AMySFC_EnemyBase : public ASFC_CharacterBase
+class STARFORGECOVENANT_API AMySFC_EnemyBase : public ASFC_CharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void HighlightActor();
+	virtual void UnHighlightActor();
 	
 };
