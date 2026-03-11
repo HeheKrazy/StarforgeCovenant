07db8e7b52891c149382fdf6ca708df012435b65
HeheKrazy <AdamSabo2750@Gmail.com>
2026-03-10 22:03:23 -0400

    Health and Energy Attributes

--- Source/StarforgeCovenant/Private/AbilitySystem/SFCAttributeSet.cpp
+++ Source/StarforgeCovenant/Private/AbilitySystem/SFCAttributeSet.cpp
@@ -2,4 +2,40 @@
 
 
 #include "AbilitySystem/SFCAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+USFCAttributeSet::USFCAttributeSet()
+{
+	
+}
+
+void USFCAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME_CONDITION_NOTIFY(USFCAttributeSet, Health, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(USFCAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(USFCAttributeSet, Energy, COND_None, REPNOTIFY_Always)
+	DOREPLIFETIME_CONDITION_NOTIFY(USFCAttributeSet, MaxEnergy, COND_None, REPNOTIFY_Always)
+}
+
+void USFCAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USFCAttributeSet, Health, OldHealth);
+}
+
+void USFCAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USFCAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void USFCAttributeSet::OnRep_Energy(const FGameplayAttributeData& OldEnergy) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USFCAttributeSet, Energy, OldEnergy);
+}
+
+void USFCAttributeSet::OnRep_MaxEnergy(const FGameplayAttributeData& OldMaxEnergy)  const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USFCAttributeSet, MaxEnergy, OldMaxEnergy);
+}
--- Source/StarforgeCovenant/Public/AbilitySystem/SFCAttributeSet.h
+++ Source/StarforgeCovenant/Public/AbilitySystem/SFCAttributeSet.h
@@ -7,11 +7,41 @@
 #include "SFCAttributeSet.generated.h"
 
 /**
+ * To add attributes, create FGameplayAttributeData variable.
+ * - create OnRep_Attribute()
+ * - Override GetLifetimeReplicatedProps()
+ * -DOREPLIFETIME_CONDITION_NOTIFY in GetLifetimeReplicatedProps()
  * 
  */
 UCLASS()
 class STARFORGECOVENANT_API USFCAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	USFCAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	/* Attributes */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Energy, Category = "Vital Attributes")
+	FGameplayAttributeData Energy;
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxEnergy, Category = "Vital Attributes")
+	FGameplayAttributeData MaxEnergy;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	UFUNCTION()
+	void OnRep_Energy(const FGameplayAttributeData& OldEnergy) const;
+	UFUNCTION()
+	void OnRep_MaxEnergy(const FGameplayAttributeData& OldMaxEnergy) const;
+	/* end Attributes */
 	
 };
