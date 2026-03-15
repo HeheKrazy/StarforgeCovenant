b9774416a42a57ce4f4d1cb84a0e39b2d96d0e17
HeheKrazy <AdamSabo2750@Gmail.com>
2026-03-15 18:14:29 -0400

    Energy Callbacks for WidgetController

--- Source/StarforgeCovenant/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/StarforgeCovenant/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -13,6 +13,8 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	OnMaxHealthChanged.Broadcast(SFCAttributeSet->GetMaxHealth());
 	OnShieldChanged.Broadcast(SFCAttributeSet->GetShield());
 	OnMaxShieldChanged.Broadcast(SFCAttributeSet->GetMaxShield());
+	OnEnergyChanged.Broadcast(SFCAttributeSet->GetEnergy());
+	OnMaxEnergyChanged.Broadcast(SFCAttributeSet->GetMaxEnergy());
 }
 
 void UOverlayWidgetController::BindCallbacksToDependencies()
@@ -34,6 +36,14 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 	//MaxShield
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(SFCAttributeSet->GetMaxShieldAttribute()).AddUObject(
 		this, &UOverlayWidgetController::MaxShieldChanged);
+	
+	//Energy
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(SFCAttributeSet->GetEnergyAttribute()).AddUObject(
+		this, &UOverlayWidgetController::EnergyChanged);
+	
+	//MaxEnergy
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(SFCAttributeSet->GetMaxEnergyAttribute()).AddUObject(
+		this, &UOverlayWidgetController::MaxEnergyChanged);
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
@@ -54,4 +64,13 @@ void UOverlayWidgetController::ShieldChanged(const FOnAttributeChangeData& Data)
 void UOverlayWidgetController::MaxShieldChanged(const FOnAttributeChangeData& Data) const
 {
 	OnMaxShieldChanged.Broadcast(Data.NewValue);
+}
+void UOverlayWidgetController::EnergyChanged(const FOnAttributeChangeData& Data) const
+{
+	OnEnergyChanged.Broadcast(Data.NewValue);
+}
+
+void UOverlayWidgetController::MaxEnergyChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxEnergyChanged.Broadcast(Data.NewValue);
 }
--- Source/StarforgeCovenant/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/StarforgeCovenant/Public/UI/WidgetController/OverlayWidgetController.h
@@ -12,6 +12,8 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, Ne
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnShieldChangedSignature, float, NewShield);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxShieldChangedSignature, float, NewMaxShield);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEnergyChangedSignature, float, NewEnergy);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxEnergyChangedSignature, float, NewMaxEnergy);
 /*end Delegates*/
 
 /**
@@ -38,12 +40,18 @@ public:
 	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
 	FOnMaxShieldChangedSignature OnMaxShieldChanged;
 	
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FOnEnergyChangedSignature OnEnergyChanged;
 	
-	
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FOnMaxEnergyChangedSignature OnMaxEnergyChanged;
+
 protected:
 	
 	void HealthChanged(const FOnAttributeChangeData& Data) const;
 	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
 	void ShieldChanged(const FOnAttributeChangeData& Data) const;
 	void MaxShieldChanged(const FOnAttributeChangeData& Data) const;
+	void EnergyChanged(const FOnAttributeChangeData& Data) const;
+	void MaxEnergyChanged(const FOnAttributeChangeData& Data) const;
 };
